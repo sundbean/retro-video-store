d1ba8019b0c0fcd89a45dde29c3986b9e2e22948
Whit Sundby <wksundby@gmail.com>
2021-05-19 09:19:48 -0700

    creates rental model

---
+++ app/models/rental.py
@@ -0,0 +1,23 @@
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from app import db
+
+class Rental(db.Model):
+    rental_id = db.Column(db.Integer, primary_key=True)
+    due_date = db.Column(db.DateTime)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.customer_id'), primary_key=True)
+    video_id = db.Column(db.Integer, db.ForeignKey('video.video_id'), primary_key=True)
+
+    def get_videos_for_customer(self, id_of_customer):
+        results = db.session.query(Customer, Video, Rental)\
+            .join(Customer, Customer.customer_id==Rental.customer_id)\
+            .join(Video, Video.video_id==Rental.video_id)\
+            .filter(Customer.customer_id==id_of_customer).all()
+        return results
+
+    def get_customers_for_video(self, id_of_video):
+        results = db.session.query(Customer, Video, Rental)\
+            .join(Customer, Customer.customer_id==Rental.customer_id)\
+            .join(Video, Video.video_id==Rental.video_id)\
+            .filter(Video.video_id==id_of_video).all()
+        return results
