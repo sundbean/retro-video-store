4a0486e5cf3cb0914d8c32e3dbbe292cadcdb3f1
Whit Sundby <wksundby@gmail.com>
2021-05-18 19:25:38 -0700

    updates Customer model, adds CRUD outline to routes, adds GET and POST methods to /customers route, adds GET method to /customers/<id> route, adds helper method for error handling

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,104 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, request, jsonify
+from app import db
+from app.models.video import Video
+from app.models.customer import Customer
+import requests
+import os
 
+videos_bp = Blueprint("videos", __name__, url_prefix="/videos")
+customers_bp = Blueprint("customers", __name__, url_prefix="/customers")
+
+#######################################################
+################ CRUD CUSTOMERS #######################
+#######################################################
+
+# Exceptions for requests
+# https://stackoverflow.com/questions/16511337/correct-way-to-try-except-using-python-requests-module
+
+# try:
+#     ...
+# except requests.exceptions.RequestException as e:
+#     ...
+
+@customers_bp.route("", methods=["GET"])
+def get_all_customers():
+    customers = Customer.query.all()
+
+    return jsonify([customer.get_customer_info() for customer in customers])
+
+
+@customers_bp.route("", methods=["POST"])
+def post_new_customer():
+    request_body = request.get_json()
+
+    try:
+        new_customer = Customer(name=request_body["name"],
+                                postal_code=request_body["postal_code"],
+                                phone=request_body["phone"])
+    except KeyError as e:
+        return make_response(detail_error("Invalid data"), 400)
+
+    db.session.add(new_task)
+    db.session.commit()
+
+    response = {
+        "id": new_customer.customer_id
+    }
+
+    return make_response(jsonify(response), 201)
+
+
+@customers_bp.route("/<customer_id>", methods=["GET"])
+def get_single_customer(customer_id):
+    customer = Customer.query.get(customer_id)
+    if customer is None:
+        return make_response(detail_error("Customer does not exist"), 404)
+
+    return jsonify(customer.get_customer_info())
+
+
+@customers_bp.route("/<customer_id>", methods=["PUT"])
+def update_customer(customer_id):
+    pass
+
+
+@customers_bp.route("/<customer_id>", methods=["DELETE"])
+def delete_customer(customer_id):
+    pass
+
+
+
+##########################################################
+###################### CRUD VIDEOS #######################
+##########################################################
+
+@videos_bp.route("", methods=["GET"])
+def get_all_videos():
+    pass
+
+@videos_bp.route("", methods=["POST"])
+def post_new_customer():
+    pass
+
+@videos_bp.route("/<video_id>", methods=["GET"])
+def get_single_video(video_id):
+    pass
+
+@videos_bp.route("/<video_id>", methods=["PUT"])
+def update_video(video_id):
+    pass
+
+@videos_bp.route("/<video_id>", methods=["DELETE"])
+def delete_video(video_id):
+    pass
+
+
+
+##################### HELPER FUNCTIONS #####################
+
+def detail_error(error):
+    return {
+        "errors": [
+            error
+        ]
+    }
--- models/customer.py
+++ models/customer.py
@@ -5,6 +5,17 @@ from app import db
 class Customer(db.Model):
     customer_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String)
-    postal_code = db.Column(db.Integer)
+    postal_code = db.Column(db.String)
     phone = db.Column(db.String)
-    register_at = db.Colum(db.Datetime)
+    registered_at = db.Column(db.Datetime)
+
+    def get_customer_info(self):
+        customer_info = {
+            "id": self.customer_id,
+            "name": self.name,
+            "registered_at": self.registered_at,
+            "postal_code": self.postal_code,
+            "phone": self.phone
+            "videos_checked_out_count": 0
+        }
+        return customer_info
